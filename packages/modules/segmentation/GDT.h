5746255d2f0b79f9b36793bfbc733d5921b193bc
Will Fantom <w.fantom@lancaster.ac.uk>
2018-03-20 15:38:05 +0000

    Updated GDT to use new data types

--- packages/modules/segmentation/GDT.h
+++ packages/modules/segmentation/GDT.h
@@ -7,12 +7,12 @@
 
 struct gdt_entry
 {
-    unsigned short limit_low;
-    unsigned short base_low;
-    unsigned char base_middle;
-    unsigned char access;
-    unsigned char granularity;
-    unsigned char base_high;
+    uint16_t limit_low;
+    uint16_t base_low;
+    uint8_t base_middle;
+    uint8_t access;
+    uint8_t granularity;
+    uint8_t base_high;
 } __attribute__((packed));
 
 struct gdt_ptr
